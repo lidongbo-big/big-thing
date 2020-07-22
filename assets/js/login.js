0343eb3686e62afad93a7a53a62fd75b243f81f9
lidongbo <1144086550@qq.com>
2020-07-22 15:15:33 +0800

    完成注册表单验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,4 +8,16 @@ $(function () {
         $('.login').show()
         $('.reg').hide()
     })
+    // 使用layui需要加载模块
+    var form = layui.form
+    // 表单验证
+    form.verify({
+        pass: [/^[\S]{6,12}$/, '密码长度必须是6~12位'],
+        repass: function (value) {
+            var pwd = $('.password').val().trim()
+            if (pwd != value) {
+                return '密码不一致'
+            }
+        }
+    });
 })
