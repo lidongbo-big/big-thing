9f48cf0d09c4b3e6649fd9a8c24370925fd76248
lidongbo <1144086550@qq.com>
2020-07-25 15:56:57 +0800

    优化拦截器的拦截内容

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
 $.ajaxPrefilter(function (option) {
-    console.log(option);
-    option.url ='http://www.liulongbin.top:3007' + option.url
+    // console.log(option);
+    option.url = 'http://www.liulongbin.top:3007' + option.url
+
+
+    if (option.url.indexOf('/my/') !== -1) {
+        option.complete = function (xhr) {
+            if (xhr.responseJSON.status === 1) {
+                location.href = '/login.html'
+            }
+        }
+        option.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -17,10 +17,25 @@ function getUserInfo () {
     $.ajax({
         url: '/my/userinfo',
         success: function (res) {
-            console.log(res);
+            // console.log(res);
+            if (res.status === 1) {
+                return  layer.msg(res.message)
+            }
+            renderHtml(res.data)
         },
-        headers: {
-            Authorization: localStorage.getItem('token')
-        }
     })
+}
+
+function renderHtml(data) {
+    var name = data.nickname || data.username
+    var firstText = name.substr(0, 1).toUpperCase();
+    
+    if (data.user_pic) {
+        $('.person img').show().attr('src', data.yser_pic)
+        $('.text-avatar').hide()
+    } else {
+        $('.person img').hide()
+        $('.text-avatar').css('display', 'inline-block').text(firstText)
+    }
+    $('.welcome').html('欢迎你&nbsp;&nbsp;' + name)
 }
